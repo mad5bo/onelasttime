fbf4765ebbd5615fe2cffb3df41bab3f26d68a1a
mad5bo <68882369+mad5bo@users.noreply.github.com>
2020-08-31 15:54:36 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,10 @@ provider "openstack" {
 }
 #Create an IBM i partition
 resource "openstack_compute_instance_v2" "PowerVC-LPAR" {
-  name        = "${var.ibm_stack_name}"  
-  image_name  = "${var.openstack_image_name}"
-  flavor_name = "${var.openstack_flavor_name}"   
-  network {
-    name = "${var.openstack_network_name}"
+  name         = "${var.ibm_stack_name}"  
+  image_name   = "${var.openstack_image_name}"
+  flavor_name  = "${var.openstack_flavor_name}"   
+  network_name = "${var.openstack_network_name}"
    }
 #Variables for deployment
 variable "openstack_image_name" {
