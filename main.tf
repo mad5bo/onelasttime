f721b8ccf4937de4e20f5cee95b38837e127313f
mad5bo <68882369+mad5bo@users.noreply.github.com>
2020-08-31 15:52:52 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+provider "openstack" {
+  insecure = true
+  version  = "~> 0.2"
+}
+#Create an IBM i partition
+resource "openstack_compute_instance_v2" "PowerVC-LPAR" {
+  name        = "${var.ibm_stack_name}"  
+  image_name  = "${var.openstack_image_name}"
+  flavor_name = "${var.openstack_flavor_name}"   
+  network {
+    name = "${var.openstack_network_name}"
+   }
+#Variables for deployment
 variable "openstack_image_name" {
   description = "The Name of the image to be used for deployment."
 }
@@ -13,22 +26,6 @@ variable "openstack_network_name" {
 variable "ibm_stack_name" {
   description = "Stack Name"
 }
-
-provider "openstack" {
-  insecure = true
-  version  = "~> 0.3"
-}
-
-resource "openstack_compute_instance_v2" "PowerVC-LPAR" {
-  name        = "${var.ibm_stack_name}"  
-  image_name  = "${var.openstack_image_name}"
-  flavor_name = "${var.openstack_flavor_name}"   
-  
-  network {
-    name = "${var.openstack_network_name}"
-    
-    }
-  
   output "single-vm-ip" {
   value = "${openstack_compute_instance_v2.PowerVC-LPAR.*.network.0.fixed_ip_v4}"
    }
